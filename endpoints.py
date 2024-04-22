51284160a637c2ca665adaee03c0be0ae10a7cd4
jqm4954 <jqm4954@rit.edu>
2024-04-22 09:50:59 -0400

    Created first GET request endpoint at /assets. Updated comments for helper methods

--- endpoints.py
+++ endpoints.py
@@ -3,9 +3,11 @@ import sqlite3
 
 app = Flask(__name__)
 
+# Helper function to return database connection
 def get_connection():
     return sqlite3.connect("assets.db")
 
+# Helper function to create DB if not already created
 def create_db():
     conn = get_connection()
     cursor = conn.cursor()
@@ -20,3 +22,16 @@ def create_db():
     conn.commit()
     conn.close()
 
+# GET request that returns all assets from the table
+@app.route('/assets', methods=['GET'])
+def get_all_assets():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM assets")
+    assets = cursor.fetchall()
+    conn.close()
+    return jsonify(assets)
+
+if __name__ == "__main__":
+    create_db()
+    app.run()
